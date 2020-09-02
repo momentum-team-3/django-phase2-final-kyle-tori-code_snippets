780bc19f83150559ace215ab486d6a300293882d
kldavis52 <kyle.linnard.davis@gmail.com>
2020-09-02 18:08:47 -0400

    Links work except logout

--- codesnippets_project/code_snippets/views.py
+++ codesnippets_project/code_snippets/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.forms import UserCreationForm
 from .models import Snippet
+from users.models import User
 from django.http import HttpResponse
 
 # Create your views here.
 def snippets(request):
-    print('Hello World')
     snippets = Snippet.objects.all()
     return render(request, 'snippets.html', {'snippets': snippets})
 
-# def userprofile(request):
-#     if not request.user.is_authenticated:
-#         return render(request, 'login_error.html')
+def userprofile(request, pk):
+    if not request.user.is_authenticated:
+        return render(request, 'login.html')
+    user = User.objects.get(pk=pk)
+    if request.user is isinstance(user, AnonymousUser):
+        return render(request, 'login.html')
+    else:
+        snippets = Snippet.objects.filter(user=user)
+        return render(request, 'userprofile.html', {'user': user, 'snippets': snippets})
